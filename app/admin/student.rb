a40c46eca6ed1706522377ff02b6cad7f221ffc1
Philip A. Nunoo <ketay99@gmail.com>
2014-09-28 12:29:04 +0000

    Work on the Student Model Took out the association between the student and course
    In your Student model removed the validation for email field thank you.
    
    Recommendation:
    Please check on the relationship between the course and student tables.
    Don't really understand the relationship that's why I left them unrelated for now.

--- app/admin/student.rb
+++ app/admin/student.rb
@@ -1,18 +1,3 @@
 ActiveAdmin.register Student do
-
-
-  # See permitted parameters documentation:
-  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-  #
-  permit_params :first_name, :last_name, :username, :phone_number, :address, :date_of_birth, :email, :school, :program_of_study, :year, :fees, :course_id
-  #
-  # or
-  #
-  # permit_params do
-  #   permitted = [:permitted, :attributes]
-  #   permitted << :other if resource.something?
-  #   permitted
-  # end
-
-
+  
 end
